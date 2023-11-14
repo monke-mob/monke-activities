5d8a30ff1e7bfe276563d70ea3642937c007894f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2023-11-13 20:59:36 -0600

    Cleanup.

--- src/server/runtime.server.lua
+++ src/server/runtime.server.lua
@@ -1,5 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+
 Knit.AddServicesDeep(script.Parent.services)
 Knit.Start():catch(warn)
