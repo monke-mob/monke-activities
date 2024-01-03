20a5a8d54fd944a085526598e12d8591d61ebb4b
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-03 12:27:21 -0600

    Fix styling again :(.

--- src/client/app/core/roundInfo/menuToggle.lua
+++ src/client/app/core/roundInfo/menuToggle.lua
@@ -3,6 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Fusion = require(ReplicatedStorage.Packages.Fusion)
 
 local button = require(script.Parent.Parent.Parent.components.button)
+local frame = require(script.Parent.Parent.Parent.components.frame)
 local menuOpenAction = require(script.Parent.Parent.Parent.actions.menu.open)
 local theme = require(script.Parent.Parent.Parent.theme)
 
@@ -12,36 +13,50 @@ local theme = require(script.Parent.Parent.Parent.theme)
 	@returns Fusion.Component
 --]]
 local function menuToggle()
-    return button({
+    return frame({
         AnchorPoint = Vector2.new(0, 0.5),
         Position = UDim2.fromScale(0.05, 0.5),
         Size = UDim2.fromScale(0.245, 0.65),
-        BackgroundTransparency = 0,
-        BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-
-        [Fusion.OnEvent("Activated")] = function()
-            menuOpenAction:set(true)
-        end,
+        BackgroundTransparency = 1,
 
         [Fusion.Children] = {
-            Fusion.New("UIGradient")({
-                Color = theme.gradient.dark,
-                Rotation = 90,
-            }),
+            button({
+                AnchorPoint = Vector2.new(1, 0.5),
+                Position = UDim2.fromScale(1, 0.5),
+                Size = UDim2.fromScale(0.245, 0.65),
+                BackgroundTransparency = 0,
+                BackgroundColor3 = Color3.fromRGB(255, 255, 255),
 
-            Fusion.New("ImageLabel")({
-                ImageColor3 = theme.foreground.light,
-                Image = "rbxassetid://14800609693",
-                Size = UDim2.fromScale(0, 0.4),
-                BackgroundTransparency = 1,
+                [Fusion.OnEvent("Activated")] = function()
+                    menuOpenAction:set(true)
+                end,
 
                 [Fusion.Children] = {
                     Fusion.New("UIAspectRatioConstraint")({
                         AspectType = Enum.AspectType.ScaleWithParentSize,
                         DominantAxis = Enum.DominantAxis.Height,
                     }),
+
+                    Fusion.New("UIGradient")({
+                        Color = theme.gradient.dark,
+                        Rotation = 90,
+                    }),
+
+                    Fusion.New("ImageLabel")({
+                        ImageColor3 = theme.foreground.light,
+                        Image = "rbxassetid://14800609693",
+                        Size = UDim2.fromScale(0, 0.4),
+                        BackgroundTransparency = 1,
+
+                        [Fusion.Children] = {
+                            Fusion.New("UIAspectRatioConstraint")({
+                                AspectType = Enum.AspectType.ScaleWithParentSize,
+                                DominantAxis = Enum.DominantAxis.Height,
+                            }),
+                        },
+                    }),
                 },
-            }),
+            }, {}),
         },
     }, {})
 end
