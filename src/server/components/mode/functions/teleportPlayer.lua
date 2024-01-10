799056a6dd441e05f37c077527348edf2cdcabad
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-09 21:34:36 -0600

    Fix teleport height correction.

--- src/server/components/mode/functions/teleportPlayer.lua
+++ src/server/components/mode/functions/teleportPlayer.lua
@@ -10,9 +10,8 @@ local Players = game:GetService("Players")
 local function freezePlayer(userID: number, position: CFrame)
     local player: Player = Players:GetPlayerByUserId(userID)
     local character: Model = player.Character or player.CharacterAdded:Wait()
-    local characterHumanoid: Humanoid = player:FindFirstChildOfClass("Humanoid") :: any
-    -- Adding the hip height makes the feet be at the teleport point instead of the torso.
-    character:PivotTo(position + Vector3.new(0, characterHumanoid.HipHeight, 0))
+    -- Adding the half of the height makes the feet be at the teleport point instead of the torso.
+    character:PivotTo(position + Vector3.new(0, character:GetPivot().Y / 2, 0))
 end
 
 return freezePlayer
