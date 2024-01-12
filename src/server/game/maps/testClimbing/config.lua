7bc2869f30f1047e6cb6bd51529fcf79159e05ce
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-12 14:39:42 -0600

    Change src path.

--- src/server/game/maps/testClimbing/config.lua
+++ src/server/game/maps/testClimbing/config.lua
@@ -3,7 +3,7 @@ local mapTypes = require(script.Parent.Parent.Parent.Parent.components.map.types
 return {
     id = "testClimbing",
 
-    src = script.Parent.src.Model,
+    src = game.ServerStorage.Model,
 
     compatibleModes = {
         "climbing",
