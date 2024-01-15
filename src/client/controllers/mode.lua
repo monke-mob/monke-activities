a915368b4143a9b8d279e62a95a8e2a05248df2b
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-14 22:31:48 -0600

    Fix method destruction.

--- src/client/controllers/mode.lua
+++ src/client/controllers/mode.lua
@@ -82,7 +82,7 @@ function modeController:_remove()
         return
     end
 
-    self._current:destroy()
+    self._current.mode:destroy()
     self._current.mode = nil
     self._current.config = nil
 end
