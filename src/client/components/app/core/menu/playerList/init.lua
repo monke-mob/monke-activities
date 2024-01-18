b0b03a474df5141c9058e3281a239524c1d243f7
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-01-18 10:34:13 -0600

    Starts #58.

---
+++ src/client/components/app/core/menu/playerList/init.lua
@@ -0,0 +1,12 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Fusion = require(ReplicatedStorage.Packages.Fusion)
+
+--[[
+	Handles the player list.
+
+	@returns Fusion.Component
+--]]
+local function playerList() end
+
+return playerList
