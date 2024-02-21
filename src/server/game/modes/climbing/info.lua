9f126603379e96b3d15991e36ae4d2d01c0ebced
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-02-21 07:27:54 -0600

    Improve mode name & description.

--- src/server/game/modes/climbing/info.lua
+++ src/server/game/modes/climbing/info.lua
@@ -3,7 +3,7 @@ local modeTypes = require(script.Parent.Parent.Parent.Parent.components.mode.typ
 
 return {
     id = config.id,
-    name = "climbing",
+    name = "Ascend",
     type = "single",
-    description = "Who can go the farest?",
+    description = "Ascend to new heights within a time limit, dodging obstacles and outmaneuvering fallen climbers. How high can you go?",
 } :: modeTypes.info
